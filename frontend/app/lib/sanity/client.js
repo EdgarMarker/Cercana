315985ed1dd9b8a3571f4e1b42c99dbc0320f78d
Edgar <jorge260590@hotmail.com>
2025-04-30 13:02:34 -0600

    minor issue

--- frontend/app/lib/sanity/client.js
+++ frontend/app/lib/sanity/client.js
@@ -1,8 +1,8 @@
 import { createClient } from "@sanity/client";
 
 export const client = createClient({
-  projectId: process.env.NEXT_PUBLIC_STUDIO_PROJECT_ID,
-  dataset: process.env.NEXT_PUBLIC_STUDIO_DATASET,
+  projectId: process.env.SANITY_STUDIO_PROJECT_ID,
+  dataset: process.env.SANITY_STUDIO_DATASET,
   apiVersion: "2025-04-29",
   // Set to `true` for production environments
   useCdn: false,
